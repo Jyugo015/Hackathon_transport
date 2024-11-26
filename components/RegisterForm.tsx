3ae235c25f20fb000f138aa2e3e33f339e14fc96
HamZhiWei33 <149857735+HamZhiWei33@users.noreply.github.com>
2024-11-26 12:26:15 +0800

    create draft register page

---
+++ components/RegisterForm.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import Link from "next/link";
+import { useState, FormEvent } from "react";
+import { useRouter } from "next/navigation";
+
+const RegisterForm: React.FC = () => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      setError("All fields are necessary.");
+      return;
+    }
+
+    // try {
+    //   const resUserExists = await fetch("api/userExists", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ email }),
+    //   });
+
+    //   const { user } = await resUserExists.json();
+
+    //   if (user) {
+    //     setError("User already exists.");
+    //     return;
+    //   }
+
+    //   const res = await fetch("api/register", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ name, email, password }),
+    //   });
+
+    //   if (res.ok) {
+    //     const form = e.target as HTMLFormElement;
+    //     form.reset();
+    //     router.push("/");
+    //   } else {
+    //     setError("User registration failed.");
+    //   }
+    // } catch (error) {
+    //   console.error("Error during registration:", error);
+    //   setError("An error occurred. Please try again later.");
+    // }
+  };
+
+  return (
+    <div className="grid place-items-center h-screen">
+      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+        <h1 className="text-xl font-bold my-4">Register</h1>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Full Name"
+            className="border p-2 rounded-md"
+          />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+            className="border p-2 rounded-md"
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+            className="border p-2 rounded-md"
+          />
+          <button
+            type="submit"
+            className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2 rounded-md"
+          >
+            Register
+          </button>
+
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
+
+          <Link className="text-sm mt-3 text-right" href={"/"}>
+            Already have an account? <span className="underline">Login</span>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterForm;
