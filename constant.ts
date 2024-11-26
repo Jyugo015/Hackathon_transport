4607987d920da2ec7b4be054b02305f3fa1d682f
HamZhiWei33 <149857735+HamZhiWei33@users.noreply.github.com>
2024-11-26 12:29:26 +0800

    create menu item list

--- constant.ts
+++ constant.ts
@@ -4,6 +4,7 @@ export type MenuItem = {
 };
 
 export const MENU_ITEMS: MenuItem[] = [
+  { name: "Home", link: "/" },
   { name: "PageOne", link: "/pageone" },
   { name: "PageTwo", link: "/pagetwo" },
   { name: "PageThree", link: "/pagethree" },
