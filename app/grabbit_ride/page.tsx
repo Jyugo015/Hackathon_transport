3d905c405462cd8f22fd43584d33530fdc4038dc
HamZhiWei33 <149857735+HamZhiWei33@users.noreply.github.com>
2024-11-27 22:44:09 +0800

    create initial grabbit ride page

---
+++ app/grabbit_ride/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Grabbit Ride</h1>
+      <p className="h-screen">This is the content for book ride page.</p>
+    </div>
+  );
+};
+
+export default Page;
