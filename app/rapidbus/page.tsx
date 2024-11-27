139471f591477af9d1e6bea735a0641e1f353508
HamZhiWei33 <149857735+HamZhiWei33@users.noreply.github.com>
2024-11-27 22:51:50 +0800

    create rapid bus page

--- app/rapidbus/page.tsx
+++ app/rapidbus/page.tsx
@@ -1,8 +1,8 @@
 const PageOne = () => {
   return (
     <div>
-      <h1>Page Two</h1>
-      <p>This is the content for Page Two.</p>
+      <h1>Rapid Bus Page</h1>
+      <p className="h-screen">This is the content for Rapid Bus Page.</p>
     </div>
   );
 };
